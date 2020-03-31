bfcc653ce5bd33943cfe090f9b365b894eedf7e3
root <root@vps759568.ovh.net>
2020-03-31 15:13:41 +0200

    Yolo

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var app = express();
+
+app.get("/webhooks/github", function(req, res) {
+    res.json({msg: "This is the get request yo retard"});
+})
+
+app.post("/webhooks/github", function (req, res) {
+    console.log("Received update :", req.body);
+
+})
+
+app.listen(3000, function() {
+    console.log("Server is running and waiting for connections");
+})
