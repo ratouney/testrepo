14e3c90de3841d13019f0b47a34ba3958f95d444
root <root@vps759568.ovh.net>
2020-03-31 15:22:43 +0200

    added body parser

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var express = require("express");
+var bodyParser = require('body-parser')
 var app = express();
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
 
 let temp = {msg: "Nothing yet"};
 
